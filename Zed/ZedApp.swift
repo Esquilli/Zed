988c43952e35f28d6bd848f0f7a9ebfc95d7f056
Pedro J. Fernandez <esquillidev@gmail.com>
2022-01-19 16:40:51 -0500

    Initial Commit

---
+++ Zed/ZedApp.swift
@@ -0,0 +1,20 @@
+//
+//  ZedApp.swift
+//  Zed
+//
+//  Created by Pedro Fernandez on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct ZedApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
